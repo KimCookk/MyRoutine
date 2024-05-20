19acfab829d0a2cf40a3e4cf32981a0e444c2af7
Kim TaeSung <fopc4131@gmail.com>
2024-05-20 22:14:07 +0900

    Project Init, gitignore Add

---
+++ MyRoutine/MyRoutine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyRoutine
+//
+//  Created by 김태성 on 5/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyRoutine/MyRoutine/MyRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRoutineApp.swift
+//  MyRoutine
+//
+//  Created by 김태성 on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MyRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
